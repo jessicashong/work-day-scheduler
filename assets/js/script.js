ef9c7dd273d109c7d1b937dbdea91255daa892f5
jessicashong <jessicashong12@gmail.com>
2022-11-12 12:27:02 -0600

    add function to submit textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 var date = document.getElementById('currentDay');
 var timeSlots = document.querySelector('.container');
-
+var textEntry = document.querySelector('.event');
+var saveBtn = document.querySelector('.saveBtn');
 
 var currentDate = moment();
 $("#currentDay").text(currentDate.format('dddd, MMMM Do'));
+
+textEntry.value = localStorage.getItem('agenda');
+
+function sumbitAgenda(event){
+    event.preventDefault();
+    textEntry = $('textarea[name="text"]').val();
+    console.log(textEntry);
+
+    localStorage.setItem('agenda', textEntry);
+
+}
+saveBtn.addEventListener('click', sumbitAgenda);
+
 
